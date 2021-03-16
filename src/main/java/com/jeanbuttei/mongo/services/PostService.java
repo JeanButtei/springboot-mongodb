73520b90d3e6b8152e01782dae23753b21cc1a00
JeanButtei <jeanbuttei@gmail.com>
2021-03-16 11:08:12 -0300

    Obtendo um post por id

---
+++ src/main/java/com/jeanbuttei/mongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.jeanbuttei.mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jeanbuttei.mongo.domain.Post;
+import com.jeanbuttei.mongo.repository.PostRepository;
+import com.jeanbuttei.mongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
